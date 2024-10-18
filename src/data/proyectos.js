ee05377cea8f479dfaa0dc10f87eec0d70bd4562
josue <alejandro@MacBook-Air-de-josue.local>
2024-10-18 17:50:21 -0600

    Update Proyectos.astro

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -3,27 +3,27 @@ export const proyectos = [
         id: 1,
         title: "Crud",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "",
+        iconGithub: "/public/img/icons/tailwind.png",
     },
 
     {   
         id: 2,
         title: "Listado de Tareas",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "",
+        iconGithub: "/public/img/icons/tailwind.png",
     },
 
     {   
         id: 3,
         title: "Carrito de compras",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "",
+        iconGithub: "/public/img/icons/tailwind.png",
     },
 
     {
         id: 4,
         title: "Galery App",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "",
+        iconGithub: "/public/img/icons/tailwind.png",
     },
 ]
