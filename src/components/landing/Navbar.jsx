b27ea8703f7817676e3fc783b03fc2522923ec2c
josue <alejandro@MacBook-Air-de-josue.local>
2024-10-17 18:10:32 -0600

    Se agrega responsive design en Header.astro

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="bg-gradient-to-r from-[#0d161c] to-[#1e324d] shadow-2xl sm:flex justify-between py-5">
-            <div className="flex justify-between px-3">
-                <div className="cursor-pointer ml-4 duration-300 ">
-                    <img src={logo.src} alt="" className='h-14 animate-pulse'/>
+        <nav className="bg-gradient-to-r from-[#0d161c] to-[#1e324d] shadow-2xl sm:flex justify-between items-center py-5 sm:px-12">
+            <div className="flex items-center justify-between px-3">
+                <div className="cursor-pointer ml-4">
+                    <img src={logo.src} alt="" className='h-16 animate-pulse'/>
                 </div>
 
                 <div className='p-2 cursor-pointer sm:hidden'>
@@ -31,14 +31,12 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className={`bg-gradient-to-r from-[#0d161c] to-[#1e324d] p-6 duration-300 ease-in absolute top-[-400px] sm:top-0 sm:w-auto left-0 
-                w-full sm:static ${menuOpen && 'top-[80px] opacity-100'}`}>
-                <ul className='flex flex-col gap-5 sm:flex-row'>
+            <div className={`bg-gradient-to-r from-[#0d161c] to-[#1e324d] p-6 duration-300 ease-in absolute top-[-400px] sm:top-0 sm:w-auto left-0 w-full sm:static sm:bg-none ${menuOpen && 'top-[80px] opacity-100'}`}>
+                <ul className='flex flex-col gap-5 sm:flex-row sm:gap-7'>
                     <li className='text-white font-bold tracking-wide'><a href="#Contacto" onClick={ closeMenu}>Sobre Mi</a></li>
                     <li className='text-white font-bold tracking-wide'><a href="#Tecnologias" onClick={ closeMenu}>Tecnologias</a></li>
                     <li className='text-white font-bold tracking-wide'><a href="#Proyectos" onClick={ closeMenu}>Proyectos</a></li>
                     <li className='text-white font-bold tracking-wide'><a href="#Contacto" onClick={ closeMenu}>Contacto</a></li>
-                   
                 </ul>
             </div>        
         </nav>
