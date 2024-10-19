496ab5c913b575cb7e3e00a69e89c39aa6804269
josue <alejandro@MacBook-Air-de-josue.local>
2024-10-18 20:12:03 -0600

    Se agregan carpeta de iconos

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -1,29 +1,37 @@
 export const proyectos = [
     {   
         id: 1,
-        title: "Crud",
+        title: "Crud - Usuarios",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "/public/img/icons/tailwind.png",
+        github: "/public/img/icons/github.png",
+        icons: [
+            {icon: "/public/img/icons/bootstrap.png"},
+            {icon: "/public/img/icons/react.png"},
+            {icon: "/public/img/icons/javascript.png"},
+        ]
     },
 
     {   
         id: 2,
         title: "Listado de Tareas",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "/public/img/icons/tailwind.png",
+        github: "/public/img/icons/github.png",
+        icons: [
+            {icon: "/public/img/icons/tailwind.png"},
+            {icon: "/public/img/icons/react.png"},
+            {icon: "/public/img/icons/javascript.png"},
+        ]
     },
 
     {   
         id: 3,
         title: "Carrito de compras",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "/public/img/icons/tailwind.png",
     },
 
     {
         id: 4,
         title: "Galery App",
         img: "/public/img/proyectos_image.png",
-        iconGithub: "/public/img/icons/tailwind.png",
     },
 ]
