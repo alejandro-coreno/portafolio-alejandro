9277b82a4f68c3d8f7868edbd018554b3586af39
josue <alejandro@MacBook-Air-de-josue.local>
2024-10-21 15:44:03 -0600

    Se agrega iconos en componentes Tecnologias.astro y se agrega Contacto.astro

---
+++ src/data/tecnologias.js
@@ -0,0 +1,16 @@
+
+export const tecnologiasFront = [
+    {icon: "/public/img/icons/react.png"},
+    {icon: "/public/img/icons/javascript.png"},
+    {icon: "/public/img/icons/tailwind.png"},
+    {icon: "/public/img/icons/typescript.png"},
+    {icon: "/public/img/icons/angular.png"},
+]
+
+export const tecnologiasBack = [
+    {icon: "/public/img/icons/node.png"},
+    {icon: "/public/img/icons/mysql.png"},
+    {icon: "/public/img/icons/git.png"},
+    {icon: "/public/img/icons/firebase.png"},
+    {icon: "/public/img/icons/java.png"},
+]
