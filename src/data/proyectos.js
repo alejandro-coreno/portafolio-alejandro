06b047c296aff9a29075c4c4ba4da1ac7bf9b888
josue <alejandro@MacBook-Air-de-josue.local>
2024-10-21 13:14:21 -0600

    Se agregan imagenes en el componente Proyectos.astro

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -2,8 +2,10 @@ export const proyectos = [
     {   
         id: 1,
         title: "Crud - Usuarios",
-        img: "/public/img/proyectos_image.png",
-        github: "/public/img/icons/github.png",
+        img: "/public/img/crud_usuarios.png",
+        github_icon: "/public/img/icons/github.png",
+        github_url: "https://github.com/alejandro-coreno/CRUD_REACT",
+        url_demo:"https://crud-usuarios-react.netlify.app/",
         icons: [
             {icon: "/public/img/icons/bootstrap.png"},
             {icon: "/public/img/icons/react.png"},
@@ -14,24 +16,42 @@ export const proyectos = [
     {   
         id: 2,
         title: "Listado de Tareas",
-        img: "/public/img/proyectos_image.png",
-        github: "/public/img/icons/github.png",
+        img: "/public/img/lista_tareas.png",
+        github_icon: "/public/img/icons/github.png",
+        github_url: "https://github.com/alejandro-coreno/Todo-List",
+        url_demo: "https://todo-list-tareas.netlify.app/",
         icons: [
-            {icon: "/public/img/icons/tailwind.png"},
             {icon: "/public/img/icons/react.png"},
             {icon: "/public/img/icons/javascript.png"},
+            {icon: "/public/img/icons/tailwind.png"},
         ]
     },
 
     {   
         id: 3,
         title: "Carrito de compras",
-        img: "/public/img/proyectos_image.png",
+        img: "/public/img/carrito.png",
+        github_icon: "/public/img/icons/github.png",
+        github_url: "https://github.com/alejandro-coreno/app-carrito",
+        url_demo: "https://app-cart-js.netlify.app/",
+        icons: [
+            {icon: "/public/img/icons/react.png"},
+            {icon: "/public/img/icons/tailwind.png"},
+            {icon: "/public/img/icons/javascript.png"},
+        ]
     },
 
     {
         id: 4,
         title: "Galery App",
-        img: "/public/img/proyectos_image.png",
+        img: "/public/img/galery_app.png",
+        github_icon: "/public/img/icons/github.png",
+        github_url: "https://github.com/alejandro-coreno/Galery-App",
+        url_demo: "",
+        icons: [
+            {icon: "/public/img/icons/firebase.png"},
+            {icon: "/public/img/icons/react.png"},
+            {icon: "/public/img/icons/typescript.png"}
+        ]
     },
 ]
